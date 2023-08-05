969ca86a210c10a01c982790884b727b3dc9241e
Mustopha Qomorudeen <adewunmi99@gmail.com>
2023-08-05 17:45:23 +0100

    UI fixes and styling

--- src/main-page/header.js
+++ src/main-page/header.js
@@ -1,12 +1,17 @@
 import logo from "./GloboLogo.png";
+import { useNavigate } from "react-router-dom";
 
 import React from "react";
 
 const Header = ({ subtitle }) => {
+  const navigate = useNavigate();
+  const loadHome = () => {
+    navigate("/");
+  };
   return (
     <header className="row">
       <div className="col-md-5">
-        <img src={logo} alt="logo" className="logo" />
+        <img src={logo} alt="logo" className="logo" onClick={loadHome} />
       </div>
       <div className="col-md-7 mt-5 subtitle">{subtitle}</div>
     </header>
